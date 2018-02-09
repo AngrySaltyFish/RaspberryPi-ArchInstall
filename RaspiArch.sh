7082afb6c862e08fde5c072afb515d08b8cdb184
Matt <TheDraca@users.noreply.github.com>
2018-02-09 21:37:38 +0000

    Minimising variables

--- RaspiArch.sh
+++ RaspiArch.sh
@@ -9,17 +9,15 @@ echo "Devices found:"
 lsblk
 echo""
 read -p "Enter device name to install to: " ArchPiDevice
-ArchPiDevice="/dev/$ArchPiDevice"
-ArchPiDevicep1=""$ArchPiDevice"p1"
-ArchPiDevicep2=""$ArchPiDevice"p2"
+ArchPiDevice="/dev/${ArchPiDevice}"
 
-if [[ $ArchPiDevice == sd* ]]; then
+if [[ ${ArchPiDevice} == sd* ]]; then
   echo "Don't install it to a HDD dumb ass"
   exit
 fi
 
-echo "[1/6] Getting read to install to $ArchPiDevice"
-sudo umount $ArchPiDevice* &>/dev/null
+echo "[1/6] Getting read to install to ${ArchPiDevice}"
+sudo umount ${ArchPiDevice}* &>/dev/null
 sudo rm -r .ArchPiSDTemp &>/dev/null
 sudo mkdir .ArchPiSDTemp
 cd .ArchPiSDTemp
@@ -44,22 +42,22 @@ echo "[2/6] Setting up device paritions"
   echo ""
   #echo Y
   echo w
-) | sudo fdisk $ArchPiDevice &>/dev/null
+) | sudo fdisk ${ArchPiDevice} &>/dev/null
 
 echo "[3/6] Creating new file system + mounting it"
 ##File system + Mounting##
-sudo mkfs.vfat $ArchPiDevicep1 &>/dev/null
+sudo mkfs.vfat ${ArchPiDevice}p1 &>/dev/null
 sudo mkdir ArchPiSDBoot
-sudo mount $ArchPiDevicep1 ArchPiSDBoot >/dev/null
+sudo mount ${ArchPiDevice}p1 ArchPiSDBoot >/dev/null
 
-sudo mkfs.ext4 $ArchPiDevicep2 &>/dev/null
+sudo mkfs.ext4 ${ArchPiDevice}p2 &>/dev/null
 sudo mkdir ArchPiSDRoot
-sudo mount $ArchPiDevicep2 ArchPiSDRoot >/dev/null
+sudo mount ${ArchPiDevice}p2 ArchPiSDRoot >/dev/null
 
 echo "[4/6] Downloading lastest image (This might take a while...)"
 ##Install ArchArm##
 sudo wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz -O  ArchLinuxArm-rpi-latest.tar.gz &>/dev/null
-echo "[5/6] Installing image to $ArchPiDevice (This will take a while!)"
+echo "[5/6] Installing image to ${ArchPiDevice} (This will take a while!)"
 sudo bsdtar -xpf ArchLinuxArm-rpi-latest.tar.gz -C ArchPiSDRoot >/dev/null
 sudo sync
 sudo mv ArchPiSDRoot/boot/* ArchPiSDBoot
@@ -75,4 +73,4 @@ sudo rm -r .ArchPiSDTemp
 echo "Install complete "
 echo "Default usernames: alarm, root"
 echo "Default passwords: alarm, root"
-notify-send "Raspberry Pi ArchInstall" "Installtion to $ArchPiDevice is complete!"
+notify-send "Raspberry Pi ArchInstall" "Installtion to ${ArchPiDevice} is complete!"
