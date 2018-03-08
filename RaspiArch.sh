cdda4672baade364e74cac8dbbbfc0401f1792ab
Matt <TheDraca@users.noreply.github.com>
2018-03-08 21:39:02 +0000

    Removed broken sudo calls and fixed fdisk
    Script must now be run as root

--- RaspiArch.sh
+++ RaspiArch.sh
@@ -17,58 +17,59 @@ if [[ ${ArchPiDevice} == sd* ]]; then
 fi
 
 echo "[1/6] Getting read to install to ${ArchPiDevice}"
-sudo umount ${ArchPiDevice}* &>/dev/null
-sudo rm -r .ArchPiSDTemp &>/dev/null
-sudo mkdir .ArchPiSDTemp
+umount ${ArchPiDevice}* &>/dev/null
+rm -r .ArchPiSDTemp &>/dev/null
+mkdir .ArchPiSDTemp
 cd .ArchPiSDTemp
 
 echo "[2/6] Setting up device paritions"
 ##SetupPartitons##
-(
-  echo o
-  echo p
-  echo n
-  echo p
-  echo 1
-  echo " "
-  echo +100M
-  #echo Y
-  echo t
-  echo c
-  echo n
-  echo p
-  echo 2
-  echo ""
-  echo ""
-  #echo Y
-  echo w
-) | sudo fdisk ${ArchPiDevice} &>/dev/null
+
+echo "o
+p
+n
+p
+1
+
++100M
+Y
+t
+c
+n
+p
+2
+
+
+Y
+w
+" | fdisk ${ArchPiDevice} &>/dev/null
+
 
 echo "[3/6] Creating new file system + mounting it"
 ##File system + Mounting##
-sudo mkfs.vfat ${ArchPiDevice}p1 &>/dev/null
-sudo mkdir ArchPiSDBoot
-sudo mount ${ArchPiDevice}p1 ArchPiSDBoot >/dev/null
+mkfs.vfat ${ArchPiDevice}p1 &>/dev/null
+mkdir ArchPiSDBoot
+mount ${ArchPiDevice}p1 ArchPiSDBoot >/dev/null
 
-sudo mkfs.ext4 ${ArchPiDevice}p2 &>/dev/null
-sudo mkdir ArchPiSDRoot
-sudo mount ${ArchPiDevice}p2 ArchPiSDRoot >/dev/null
+mkfs.ext4 ${ArchPiDevice}p2 &>/dev/null
+mkdir ArchPiSDRoot
+mount ${ArchPiDevice}p2 ArchPiSDRoot >/dev/null
 
 echo "[4/6] Downloading lastest image (This might take a while...)"
 ##Install ArchArm##
-sudo wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz -O  ArchLinuxArm-rpi-latest.tar.gz &>/dev/null
-echo "[5/6] Installing image to ${ArchPiDevice} (This will take a while!)"
-sudo bsdtar -xpf ArchLinuxArm-rpi-latest.tar.gz -C ArchPiSDRoot >/dev/null
-sudo sync
-sudo mv ArchPiSDRoot/boot/* ArchPiSDBoot
+wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz -O  ArchLinuxArm-rpi-latest.tar.gz &>/dev/null
+echo "[5/6] Installing image to ${ArchPiDevice} (This will also take a while!)"
+bsdtar -xpf ArchLinuxArm-rpi-latest.tar.gz -C ArchPiSDRoot >/dev/null
+sync
+mv ArchPiSDRoot/boot/* ArchPiSDBoot
 
 echo "[6/6] Finishing up"
 ##Finishing UP##
-sudo sync
-sudo umount ArchPiSDBoot
-sudo umount ArchPiSDRoot
+sync
+umount ArchPiSDBoot
+umount ArchPiSDRoot
 cd "$BINDER"
-sudo rm -r .ArchPiSDTemp
+rm -r .ArchPiSDTemp
 
 echo "Install complete "
 echo "Default usernames: alarm, root"
